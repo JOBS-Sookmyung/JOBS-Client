2611f621971398e01a486acd145155f60844c5a9
daljmj <126189963+daljmj@users.noreply.github.com>
2024-11-21 01:25:15 +0900

    Feat: 메인페이지, 정보입력페이지

--- jobs/src/App.js
+++ jobs/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Main from './components/mainPage/Main';
-import Input from './components/inputPage/Input';
+import Home from './pages/Home/Home';
+import Input from './pages/Input/Input';
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<Home />} />
         <Route path="/input" element={<Input />} />
       </Routes>
     </Router>
